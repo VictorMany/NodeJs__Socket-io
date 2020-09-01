d43fcb88b5d56573f62b8b53f39d8a8f0ce36841
VyctorMany <victor.velazfuentez@gmail.com>
2020-09-01 11:31:30 -0500

    Segunda practica

---
+++ 1-.socket-io/bin/dist/server.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var io = require('socket.io')();
+
+var colores = {
+  rojo: 255,
+  verde: 255,
+  zul: 255
+};
+io.sockets.on('connection', function (socket) {
+  console.log('un nuevo cliente se ha conectado' + socket.id);
+  socket.emit("actualizacion", colores);
+  socket.on('cambio', function (data) {
+    colores = data;
+    socket.broadcast.emit("actualizacion", colores);
+  });
+});
+module.exports = io;
---
+++ 1-.socket-io/public/js/client.js
@@ -0,0 +1,50 @@
+var socket = io('http://localhost:3000')
+
+
+/*
+var lblRojo = document.getElementById('lblRojo');
+var lblVerde = document.getElementById('lblVerde');
+var lblAzul = document.getElementById('lblAzul');
+
+var sldRojo = document.getElementById('sldRojo');
+var sldVerde = document.getElementById('sldVerde');
+var sldAzul = document.getElementById('sldAzul');
+
+var body = document.getElementById('body');
+
+body.style = "background-color: rgb(100, 100, 100)";
+
+lblRojo.innerHTML = "Rojo: " + sldRojo.value;
+lblVerde.innerHTML = "Verde: " + sldVerde.value;
+lblAzul.innerHTML = "Azul: " + sldAzul.value;
+
+var cambioColor = () => {
+    lblRojo.innerHTML = "Rojo: " + sldRojo.value;
+    lblVerde.innerHTML = "Verde: " + sldVerde.value;
+    lblAzul.innerHTML = "Azul: " + sldAzul.value;
+
+    body.style = "background-color: rgb("+sldRojo.value + ","+sldVerde.value+ ","+ sldAzul.value + ")";
+
+    var colores = {
+       rojo: sldRojo.value,
+       verde: sldVerde.value,
+       azul: sldAzul.value
+    }
+
+    socket.emit('cambio',colores)
+}
+
+socket.on('actualizacion', function(data){
+    lblRojo.innerHTML = "Rojo: " + data.rojo;
+    lblVerde.innerHTML = "Verde: " + data.verde;
+    lblAzul.innerHTML = "Azul: " + data.azul;
+
+    sldRojo.value= data.rojo;
+    sldVerde.value = data.verde;
+    sldAzul.value = data.azul;
+
+    body.style = "background-color: rgb("+sldRojo.value + ","+sldVerde.value+ ","+ sldAzul.value + ")";
+
+
+})
+*/
